c0534ef02619c3db7d79ea4fb6cd0c95c01f13f4
weaming <garden.yuen@gmail.com>
2023-09-24 23:32:16 +0800

    add bit.com prices

--- price_tracker.py
+++ price_tracker.py
@@ -7,7 +7,7 @@ import device
 from epaper7in5b import EPD, white, black, EPD_WIDTH as w, EPD_HEIGHT as h
 
 
-def get_symbol_price():
+def get_hope_prices():
     """
     {
         "hope_price_list": [
@@ -54,10 +54,32 @@ def get_lt_price():
         return '--'
 
 
+def bit_com_prices():
+    ret = {}
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        res = requests.request('GET', 'https://api.bit.com/um/v1/index_price?quote_currency=USD', headers=headers)
+        response = res.json()
+        for x in response['data']:
+            ret[x['index_name'].split('-')[0]] = x['index_price']
+    except Exception as e:
+        print(e)
+    return ret
+
+
 def get_vars() -> dict:
-    btc, eth, hope, ts = get_symbol_price()  # 延迟比 dexscreener 更低
+    btc, eth, hope, ts = get_hope_prices()  # 延迟比 dexscreener 更低
     lt = get_lt_price()
-    return dict(ts=ts, btc=btc, eth=eth, hope=hope, lt=lt)
+    bitcom = bit_com_prices()
+    return dict(
+        ts=ts,
+        btc=btc,
+        eth=eth,
+        hope=hope,
+        lt=lt,
+        fil=bitcom.get('FIL', '--'),
+        ton=bitcom.get('TON', '--'),
+    )
 
 
 def ts_as_datetime_str(ts):
@@ -87,11 +109,12 @@ def utime_secs_to_epoch_secs(ts):
 
 X_LEFT = 30
 Y_TOP = 20
-Y_BODY = Y_TOP + 100
+Y_BODY = Y_TOP + 90
 BODY_LEFT = X_LEFT + 120
-BODY_LINE_HEIGHT = 55
+BODY_LINE_HEIGHT = 50
+
 FONT_TITLE = 8
-FONT_BODY = 5
+FONT_BODY = 4
 
 
 def display(epd: EPD):
@@ -103,24 +126,27 @@ def display(epd: EPD):
     fb = framebuf2.FrameBuffer(device.buf, w, h, framebuf2.MHMSB)
 
     fb.fill(white)
-    fb.text('Crypto!', 400, Y_TOP, black, size=FONT_TITLE)
-    offset = 150
-    fb.text(f'{data["btc"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT, black, size=FONT_BODY)
-    fb.text(f'{data["eth"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 2, black, size=FONT_BODY)
-    fb.text(f'{data["hope"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 3, black, size=FONT_BODY)
-    fb.text(f'{data["lt"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 4, black, size=FONT_BODY)
-    epd.write_yellow_layer(device.buf, False)
-
-    fb.fill(white)
-    fb.text('Hello', X_LEFT + 50, Y_TOP, black, size=FONT_TITLE)
+    fb.text('Hello', X_LEFT + 70, Y_TOP, black, size=FONT_TITLE)
     fb.rect(0, Y_TOP + 80, w, 2, black, fill=True)
     fb.text(ts_str, X_LEFT + 200, Y_TOP + 90, black, size=3)
     fb.text(f' BTC: ', BODY_LEFT, Y_BODY + BODY_LINE_HEIGHT, black, size=FONT_BODY)
     fb.text(f' ETH: ', BODY_LEFT, Y_BODY + BODY_LINE_HEIGHT * 2, black, size=FONT_BODY)
     fb.text(f'HOPE: ', BODY_LEFT, Y_BODY + BODY_LINE_HEIGHT * 3, black, size=FONT_BODY)
     fb.text(f'  LT: ', BODY_LEFT, Y_BODY + BODY_LINE_HEIGHT * 4, black, size=FONT_BODY)
+    fb.text(f' FIL: ', BODY_LEFT, Y_BODY + BODY_LINE_HEIGHT * 5, black, size=FONT_BODY)
+    fb.text(f' TON: ', BODY_LEFT, Y_BODY + BODY_LINE_HEIGHT * 6, black, size=FONT_BODY)
+    epd.write_black_layer(device.buf, False)
 
-    epd.write_black_layer(device.buf, True)
+    fb.fill(white)
+    fb.text('Crypto!', 390, Y_TOP, black, size=FONT_TITLE)
+    offset = 150
+    fb.text(f'{data["btc"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT, black, size=FONT_BODY)
+    fb.text(f'{data["eth"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 2, black, size=FONT_BODY)
+    fb.text(f'{data["hope"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 3, black, size=FONT_BODY)
+    fb.text(f'{data["lt"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 4, black, size=FONT_BODY)
+    fb.text(f'{data["fil"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 5, black, size=FONT_BODY)
+    fb.text(f'{data["ton"]}', BODY_LEFT + offset, Y_BODY + BODY_LINE_HEIGHT * 6, black, size=FONT_BODY)
+    epd.write_yellow_layer(device.buf, True)
 
 
 def prepare():
@@ -132,6 +158,4 @@ def prepare():
 
 
 def entry():
-    if device.connect_wifi_if_not():
-        device.calibrate_time()
     display(device.epd)
